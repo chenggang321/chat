caa60bc2c17f0d2b3f5f28462fff8f66aa6c162b
chenggang321 <1129137164@qq.com>
2018-11-06 22:29:06 +0800

    react-router-dom 使用

---
+++ src/TabOne.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class TabOne extends Component {
+    render() {
+        return (
+            <div>
+                <h1>标签一</h1>
+            </div>
+        );
+    }
+}
+
+export default TabOne
--- src/index.js
+++ src/index.js
@@ -5,13 +5,27 @@ import {createStore,applyMiddleware} from 'redux'
 import {counter} from './index.redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
+import {BrowserRouter,Route,Link} from 'react-router-dom'
+import TabOne from './TabOne'
+import TabTwo from './TabTwo'
 
 
 const store = createStore(counter,applyMiddleware(thunk))
 
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+        <BrowserRouter>
+            <div>
+                <ul>
+                    <li><Link to='/'>首页</Link></li>
+                    <li><Link to='/tabOne'>标签一</Link></li>
+                    <li><Link to='/tabTow'>标签二</Link></li>
+                </ul>
+                <Route path='/' exact component={App}/>
+                <Route path='/tabOne' component={TabOne}/>
+                <Route path='/tabTow' component={TabTwo}/>
+            </div>
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root')
 );
