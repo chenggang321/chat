9277a6d4d99cf6137347dfb0612cb385ef70e9cb
chenggang321 <1129137164>
2018-11-07 09:29:13 +0800

    react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {createStore,applyMiddleware} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import {counter} from './index.redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
-import {BrowserRouter,Route,Link} from 'react-router-dom'
+import {
+    BrowserRouter,
+    Route,
+    Link,
+    Redirect,
+    Switch
+} from 'react-router-dom'
 import TabOne from './TabOne'
 import TabTwo from './TabTwo'
 
 
-const store = createStore(counter,applyMiddleware(thunk))
+const store = createStore(counter, applyMiddleware(thunk))
+
+// class Test extends React.Component {
+//     render() {
+//         console.log(this.props)
+//         return '<h2>测试组件</h2>'
+//     }
+// }
+
+// class NotFound extends React.Component {
+//     render() {
+//         return '<h2>404</h2>'
+//     }
+// }
 
 ReactDOM.render(
     <Provider store={store}>
@@ -21,9 +40,13 @@ ReactDOM.render(
                     <li><Link to='/tabOne'>标签一</Link></li>
                     <li><Link to='/tabTow'>标签二</Link></li>
                 </ul>
-                <Route path='/' exact component={App}/>
-                <Route path='/tabOne' component={TabOne}/>
-                <Route path='/tabTow' component={TabTwo}/>
+                <Switch>
+                    <Route path='/' exact component={App}/>
+                    {/*<Route path='/:id' component={Test}/>*/}
+                    <Route path='/tabOne' component={TabOne}/>
+                    <Route path='/tabTow' component={TabTwo}/>
+                    <Redirect to='/'  component={App}/>
+                </Switch>
             </div>
         </BrowserRouter>
     </Provider>,
