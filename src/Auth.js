5d6b79494d95eaf55f2427f5fc7e28b644fd1ae9
chenggang321 <1129137164@qq.com>
2018-11-07 19:40:55 +0800

    git 测试

--- src/Auth.js
+++ src/Auth.js
@@ -12,7 +12,7 @@ class Auth extends Component {
         return (
             <div>
                 {this.props.isAuth ? <Redirect to='/dashboard/'/> : null}
-                <h2>你没有权限</h2>
+                <h2>你没有权限!</h2>
                 <button onClick={this.props.login}>登陆</button>
             </div>
         )
