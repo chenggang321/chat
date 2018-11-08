c76955577ff2ec2f0aad35777af3bc7fa9f9a2c2
chenggang321 <1129137164>
2018-11-08 08:56:10 +0800

    server 修改

--- server/server.js
+++ server/server.js
@@ -1,29 +1,29 @@
 "use strict";
 const express = require("express");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const app = express();
 
-const DB_URL = "mongodb://localhost:27017/react-chat";
-mongoose.connect(DB_URL);
-mongoose.connection.on("connected", function () {
-    console.log("mongodb connect success");
-});
-
-const User = mongoose.model("user", new mongoose.Schema({
-    user: {type: String, require: true},
-    age: {type: Number, require: true}
-}));
-
-User.create({
-    user: "react-chat",
-    age: 25
-}, function (err, doc) {
-    if (!err) {
-        console.log(doc);
-    } else {
-        console.log(err);
-    }
-});
+// const DB_URL = "mongodb://localhost:27017/react-chat";
+// mongoose.connect(DB_URL);
+// mongoose.connection.on("connected", function () {
+//     console.log("mongodb connect success");
+// });
+//
+// const User = mongoose.model("user", new mongoose.Schema({
+//     user: {type: String, require: true},
+//     age: {type: Number, require: true}
+// }));
+//
+// User.create({
+//     user: "react-chat",
+//     age: 25
+// }, function (err, doc) {
+//     if (!err) {
+//         console.log(doc);
+//     } else {
+//         console.log(err);
+//     }
+// });
 
 app.get("/", function (req, res) {
     res.send("<h1>Hello World</h1>");
@@ -37,14 +37,20 @@ app.get('/data',function(req,res){
     })
 })
 
+// app.get("/data", function (req, res) {
+//     User.find({}, function (err, doc) {
+//         res.json(doc);
+//     });
+// })
+
+app.get('/data',function(req,res){
+    res.json({
+        user:'admin',
+        isAuth:false,
+        age:27
+    })
+})
+
 app.listen(8081,function(){
     console.log('server at:http://localhost:8081')
 })
-app.get("/data", function (req, res) {
-    User.find({}, function (err, doc) {
-        res.json(doc);
-    });
-})
-app.listen(8081, function () {
-    console.log("node app start at 8081");
-});
