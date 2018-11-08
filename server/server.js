3172eb47bb76fd6937eccf3c7e21cbc618752ae9
chenggang321 <1129137164>
2018-11-08 08:46:17 +0800

    server 修改

--- server/server.js
+++ server/server.js
@@ -1,10 +1,39 @@
-const express = require('express')
-const app = express()
+"use strict";
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 
-app.get('/',function(req,res){
-    res.send('Hello World!')
-})
+const DB_URL = "mongodb://localhost:27017/react-chat";
+mongoose.connect(DB_URL);
+mongoose.connection.on("connected", function () {
+    console.log("mongodb connect success");
+});
 
-app.listen(8081,function(){
-    console.log('server at:http://localhost:8081')
+const User = mongoose.model("user", new mongoose.Schema({
+    user: {type: String, require: true},
+    age: {type: Number, require: true}
+}));
+
+User.create({
+    user: "react-chat",
+    age: 25
+}, function (err, doc) {
+    if (!err) {
+        console.log(doc);
+    } else {
+        console.log(err);
+    }
+});
+
+app.get("/", function (req, res) {
+    res.send("<h1>Hello World</h1>");
+});
+
+app.get("/data", function (req, res) {
+    User.find({}, function (err, doc) {
+        res.json(doc);
+    });
 })
+app.listen(8081, function () {
+    console.log("node app start at 8081");
+});
--- src/Auth.js
+++ src/Auth.js
@@ -2,15 +2,25 @@ import React, {Component} from 'react'
 import {Redirect} from 'react-router-dom'
 import {connect} from 'react-redux'
 import {login} from './Auth.redux'
+import axios from 'axios'
 
 @connect(
     state => state.auth,
     {login}
 )
 class Auth extends Component {
+    componentDidMount(){
+        axios.get('/data').then((res) => {
+            console.log(res.data)
+            if(res.status === 200){
+                this.setState({data:res.data})
+            }
+        })
+    }
     render() {
         return (
             <div>
+                <h2>user{this.data.user}</h2>
                 {this.props.isAuth ? <Redirect to='/dashboard/'/> : null}
                 <h2>你没有权限</h2>
                 <button onClick={this.props.login}>登陆</button>
